f72cc5b8697a5de9831221b7e422b336332edaa3
reuvenz1993 <rovenroven1@gmail.com>
2019-12-12 21:08:27 +0200

    --- Auto-Git Commit ---

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , request
+from flaskext.mysql import MySQL
 
 app=Flask(__name__)
 
 @app.route('/' , methods =['GET','POST'])
 def home():
-    if
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        print ("username is :" + username)
+        print ("password is :" + password)
     return render_template("home.html")
 
 @app.route('/about/')
@@ -13,3 +18,4 @@ def about():
 
 if __name__=="__main__":
     app.run(debug=True)
+    

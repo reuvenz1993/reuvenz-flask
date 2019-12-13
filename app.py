52ad392f4c8f09672e233529f45f80c58862e7a5
reuvenz1993 <rovenroven1@gmail.com>
2019-12-13 02:12:38 +0200

    --- Auto-Git Commit ---

--- app.py
+++ app.py
@@ -5,13 +5,56 @@ from flask_mysqldb import MySQL
 import pymysql
 import mysql.connector
 import pymysql.cursors
+import MySQLdb
 
 app=Flask(__name__)
 app.config['TESTING'] = True
 
+mydb = mysql.connector.connect(
+  host="http://ec2-46-137-91-216.eu-west-1.compute.amazonaws.com",
+  user="afstpratzpjdte",
+  passwd="8fc13500ad53ae2dc352fc5adf02911a3c40b53aef1ebb02d05c39c7628b870a",
+  database="d62fast5jf6n7u"
+)
+
+print(mydb)
 
 
-connection = pymysql.connect(host='http://ec2-46-137-91-216.eu-west-1.compute.amazonaws.com/',
+conn = MySQLdb.connect('http://ec2-46-137-91-216.eu-west-1.compute.amazonaws.com', 'afstpratzpjdte', '8fc13500ad53ae2dc352fc5adf02911a3c40b53aef1ebb02d05c39c7628b870a', 'd62fast5jf6n7u')
+
+def mysqlconnect(): 
+    #Trying to connect  
+    try: 
+        db_connection= MySQLdb.connect 
+        ("http://ec2-46-137-91-216.eu-west-1.compute.amazonaws.com","afstpratzpjdte","8fc13500ad53ae2dc352fc5adf02911a3c40b53aef1ebb02d05c39c7628b870a","d62fast5jf6n7u") 
+    # If connection is not successful 
+    except: 
+        print("Can't connect to database") 
+        return 0
+    # If Connection Is Successful 
+    print("Connected") 
+  
+    # Making Cursor Object For Query Execution 
+    cursor=db_connection.cursor() 
+  
+    # Executing Query 
+    cursor.execute("SELECT CURDATE();") 
+  
+    # Above Query Gives Us The Current Date 
+    # Fetching Data  
+    m = cursor.fetchone() 
+  
+    # Printing Result Of Above 
+    print("Today's Date Is ",m[0]) 
+  
+    # Closing Database Connection  
+    db_connection.close() 
+  
+# Function Call For Connecting To Our Database 
+mysqlconnect() 
+
+
+connection = pymysql.connect(host='http://ec2-46-137-91-216.eu-west-1.compute.amazonaws.com',
                              port=5432,
                              user='afstpratzpjdte',
                              password='8fc13500ad53ae2dc352fc5adf02911a3c40b53aef1ebb02d05c39c7628b870a',
@@ -21,7 +64,7 @@ connection = pymysql.connect(host='http://ec2-46-137-91-216.eu-west-1.compute.am
  
 print(connection)
 
-
+a = MySQLdb.connect()
 
 mysql = MySQL()
 

7de951655009728b370fdc571a3679123602ff79
reuvenz1993 <rovenroven1@gmail.com>
2019-12-14 01:06:39 +0200

    --- Auto-Git Commit ---

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template , request , session
+from flask import Flask, render_template , request , session , redirect , url_for
 from flaskext.mysql import MySQL
 import MySQLdb
 from flask_mysqldb import MySQL
@@ -27,8 +27,9 @@ login_manager = LoginManager()
 
 @app.route('/' , methods =['GET','POST'])
 def home():
-    if ( session['username'] != "" ) :
-        return render_template("main.html")
+    if 'username' in session:
+        username = session['username']
+        return redirect(url_for('main'))
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -37,13 +38,13 @@ def home():
         cursor.execute("SELECT `username` ,`password` FROM `users` WHERE `username`=" +"'" + username +"'")
         res = cursor.fetchone()
         if ( res and res[1] == password ):
-            return render_template("home.html" , res = res , status = "connaction scss")
+            session['username'] = username
+            return redirect(url_for('main'))
         elif ( res ) :
             return render_template("home.html" , res = res , status = "username correct but password isnt")
         else :
             return render_template("home.html" , res = res , status = "no such username")
 
-        
     return render_template("home.html" )
 
 @app.route('/about/')
@@ -53,6 +54,12 @@ def about():
 @app.route('/main/')
 def main():
     return render_template("main.html")
+
+@app.route('/logout')
+def logout():
+   # remove the username from the session if it is there
+   session.pop('username', None)
+   return redirect(url_for('home'))
 
 if __name__=="__main__":
     app.run(debug=True)

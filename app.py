8048aa180c9f37c330d4438b73ae0bf79048389c
reuvenz1993 <rovenroven1@gmail.com>
2019-12-13 16:55:12 +0200

    --- Auto-Git Commit ---

--- app.py
+++ app.py
@@ -8,7 +8,13 @@ import pymysql.cursors
 import MySQLdb
 from MySQLdb import _mysql
 
-myDB = MySQLdb.connect(host="ec2-46-137-91-216.eu-west-1.compute.amazonaws.com",port=5432,user="afstpratzpjdte",passwd="8fc13500ad53ae2dc352fc5adf02911a3c40b53aef1ebb02d05c39c7628b870a",db="d62fast5jf6n7u")
+myDB = MySQLdb.connect(host="mysql.stackcp.com",port=53856,user="usersdb-3131357d30",passwd="usersdb-3131357d30",db="usersdb-3131357d30")
+
+myDB.query("""SELECT * FROM users """)
+
+cursor=myDB.cursor()
+cursor.execute("""SELECT * from users """)
+
 
 #db=_mysql.connect(host="outhouse",port=5432,passwd="8fc13500ad53ae2dc352fc5adf02911a3c40b53aef1ebb02d05c39c7628b870a",db="d62fast5jf6n7u")
 
@@ -26,7 +32,7 @@ def home():
         password = request.form['password']
         print ("username is :" + username)
         print ("password is :" + password)
-    return render_template("home.html")
+    return render_template("home.html" , cursor = cursor)
 
 @app.route('/about/')
 def about():

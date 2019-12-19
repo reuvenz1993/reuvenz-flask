d0273d01e9807fb5101f2ed244e5dac7b20cb636
reuvenz1993 <rovenroven1@gmail.com>
2019-12-19 04:00:55 +0200

    --- Auto-Git Commit ---

--- test.py
+++ test.py
@@ -17,11 +17,21 @@ import numpy as np
 myDB = MySQLdb.connect(host="mysql.stackcp.com",port=53856,user="usersdb-3131357d30",passwd="usersdb-3131357d30",db="usersdb-3131357d30" , charset='utf8')
 cursor=myDB.cursor()
 
-userlist_arr = []
+
 cursor.execute("SELECT username FROM `users`")
 num_of_users = cursor.rowcount
-userlist = cursor.fetchall()
-userlist = list(userlist)
+rows, cols = (num_of_users, 3) 
+arr = [[]*cols]*rows 
+print(arr) 
+userlist = list(cursor)
+
+i = 0
+while i < num_of_users :
+    arr.append(userlist[i])
+    i = i+1
+
+[x[0][:x[0].index('(')] for x in userlist]
 
 print(num_of_users)
 print(userlist)
+print(arr)

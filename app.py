84454b67ba93ffe3be2577bfb0da81aa0c287f65
reuvenz1993 <rovenroven1@gmail.com>
2019-12-20 05:08:27 +0200

    aaa

--- app.py
+++ app.py
@@ -147,13 +147,13 @@ def userlist():
 @app.route('/open_chat' , methods =['GET','POST'])
 def open_chat():
     user1 = session['username']
-    user2 = request.form['user2']
+    user2 = request.form['data']
     cursor.execute("SELECT * FROM `massages` WHERE ( sender='{}' AND receiver='{}' ) OR ( sender='{}' AND  receiver='{}'  ) ORDER BY `time` DESC".format(user1, user2 ,user2 , user1))
     temp = list( cursor.fetchall() )
     for i in range ( len(temp) ):
         temp[i] = list(temp[i])
         temp[i][4] = str(temp[i][4])
-    return json.dumps ({'data': temp})
+    return json.dumps ( temp )
 
 if __name__=="__main__":
     app.run(debug=True)

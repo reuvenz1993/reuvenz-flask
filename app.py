b7074841c27620565f5fac277368d9307e0d9884
reuvenz1993 <rovenroven1@gmail.com>
2019-12-21 18:46:40 +0200

    bb

--- app.py
+++ app.py
@@ -163,9 +163,6 @@ def add_new_post():
     cursor.execute("INSERT INTO `massages` ( `sender`, `receiver`, `massage`) VALUES ( '{}' , '{}' , '{}' )".format( user1 , user2 , text ))
     return json.dumps ( "add new post done" )
 
-
-
-
 if __name__ == '__main__':
   socketio.run( app, debug = True )
 
